ed3aedc370454d35368322bffced734c1b9553c5
lizhenguo <1016821270@qq.com>
2020-08-06 23:29:27 +0800

    StringIntroduce

---
+++ src/StringReplace/ReplaceString2.java
@@ -0,0 +1,28 @@
+package StringReplace;
+
+public class ReplaceString2 {
+    public static void main(String[] args) {
+        String str = "we are happy";
+        int count = 0;
+        for(int j = 0; j < str.length(); j++){
+            if(str.charAt(j) == ' ')//注意，char类型需要''.
+            count++;
+        }
+        char[] strReplaced = new char[str.length() + 3 * count];
+        int indexOld = str.length() - 1;
+        int indexNew = strReplaced.length - 1;
+        while (indexOld >= 0 && indexNew > indexOld){
+            if(str.charAt(indexOld) == ' '){
+                strReplaced[indexNew--] = '0';
+                strReplaced[indexNew--] = '2';
+                strReplaced[indexNew--] = '%';
+            }else{
+                strReplaced[indexNew--] = str.charAt(indexOld);
+            }
+            indexOld--;
+        }
+        //System.out.println(strReplaced.toString());//输出为: [C@1540e19d
+        String strReplacedToString = new String(strReplaced);//通过这种方式将char[]转换为String
+        System.out.println(strReplacedToString);
+    }
+}
