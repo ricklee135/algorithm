4f06b9cda88eae95607473ecb78412011b6f8dc8
lizhenguo <1016821270@qq.com>
2020-08-12 21:48:54 +0800

    QuickSort

---
+++ src/Recursive/FrogJumpStairs.java
@@ -0,0 +1,18 @@
+package Recursive;
+
+import java.util.Scanner;
+
+public class FrogJumpStairs {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int stairs = s.nextInt();
+        System.out.println(getJumpMethods(stairs));
+    }
+
+    private static int getJumpMethods(int stairs) {
+       if(stairs <= 2){
+           return stairs;
+       }
+       return getJumpMethods(stairs - 1) + getJumpMethods(stairs - 2);
+    }
+}
